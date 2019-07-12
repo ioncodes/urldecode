0a85df69e013417d8a4284c4445b63aa96d0bb13
Luca Marcelli <luca@cerrato.ch>
2019-07-13 00:57:03 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+pub struct UrlDecode { }
+
+impl UrlDecode {
+    pub fn decode(url: String) -> String {
+        let mut decoded = String::from("");
+        let mut skip = 0;
+        for i in 0..url.len() {
+            if skip != 0 {
+                skip -= 1;
+                continue;
+            }
+            let c: char = url.chars().nth(i).unwrap();
+            if c == '%' {
+                let left = url.chars().nth(i + 1).unwrap();
+                let right = url.chars().nth(i + 2).unwrap();
+                let byte = u8::from_str_radix(&format!("{}{}", left, right), 16).unwrap();
+                decoded += &(byte as char).to_string();
+                skip = 2;
+            } else {
+                decoded += &c.to_string();
+            }
+        }
+        decoded
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::UrlDecode;
+
+    #[test]
+    fn it_works() {
+        assert_eq!("https://github.com", UrlDecode::decode(String::from("https%3A%2F%2Fgithub.com")));
+    }
+}
